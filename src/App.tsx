8e974a1fe46ee1730f0aa68e2d3d7b9779e21151
skrider <skrider@berkeley.edu>
2024-02-04 20:33:59 -0800

    add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Box bg="green">
+        foo
+        </Box>
       </header>
     </div>
   );
