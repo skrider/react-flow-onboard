e65043401034408716406874a0cc3f14e5f15da8
skrider <skrider@berkeley.edu>
2024-02-04 20:37:50 -0800

    format

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@ import './App.css';
 import { Box } from '@chakra-ui/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Box bg="green">
-        foo
-        </Box>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <Box bg="green">
+                    foo
+                </Box>
+            </header>
+        </div>
+    );
 }
 
 export default App;
